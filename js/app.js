2b05f4087674653d8d69631e39e1c63e2a94351b
Asterisk Loftis <asterisk@asteriskloftis.com>
2016-06-28 16:01:51 -0600

    poop

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+//load that dat into a json object SUCKA!!!
+$.getJSON('js/AvgReqSize.json', function (response) {
+	   data = response;
+	   console.log(data.length)
+	$(window).trigger('JSONready');
+});
+
+
+$(window).on('JSONready', function () {
+	alert(data.length);
+});
+
 var scene, camera, light, renderer, controls;
 
 init();
@@ -8,26 +20,41 @@ function init() {
 
 	//Le scene AKA the world, objects, and environment stuffs
 	scene = new THREE.Scene();
-	var geometry = new THREE.OctahedronGeometry(1, 1, 1);
+	var geometry = new THREE.OctahedronGeometry(10, 1, 1);
 	var material = new THREE.MeshPhongMaterial({ color: 0x057075, specular: 0x009900, shininess: 30, shading: THREE.FlatShading });
-	var polygon = new THREE.Mesh(geometry, material);
-	scene.add(polygon);
+
+	for (var i = 0; i < data.length; i++) {
+		var polygon = new THREE.Mesh(geometry, material);
+		polygon.position.x = (Math.random() - 0.5) * 1000;
+		polygon.position.y = (Math.random() - 0.5) * 1000;
+		polygon.position.z = (Math.random() - 0.5) * 1000;
+		polygon.updateMatrix();
+		polygon.matrixAutoUpdate = false;
+		scene.add(polygon);
+				}
 
 	//Camera and TrackBall camera controls	
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	camera.position.z = 5;
 
 	controls = new THREE.TrackballControls(camera); //use TrackBall.js file
+	controls.keys = [65, 83, 68];
+
 	controls.addEventListener('change', render);
 
 	//Let there be lights
-	light = new THREE.PointLight(0x303030, 10, 100);
-	light.position.set(10, 10, 10);
-	scene.add(light);
+				light = new THREE.DirectionalLight(0xffffff);
+				light.position.set(1, 1, 1);
+				scene.add(light);
+				light = new THREE.DirectionalLight(0x002288);
+				light.position.set(-1, -1, -1);
+				scene.add(light);
+				light = new THREE.AmbientLight(0x222222);
+				scene.add(light)
 
-//the renderer the power!!!!
+	//the renderer the power!!!!
 	renderer = new THREE.WebGLRenderer();
-	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 }
