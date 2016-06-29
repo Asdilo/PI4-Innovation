efb4274624cfa8216a34cdc20aea65964d3cd25f
Asterisk Loftis <asterisk@asteriskloftis.com>
2016-06-29 15:54:25 -0600

    Tested out fonts. Too many points

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-//load that dat into a json object SUCKA!!!
+//load that data into a global json object SUCKA!!!
 $.getJSON('js/AvgReqSize.json', function (response) {
-	   data = response;
-	   console.log(data.length)
+	data = response;
+	console.log(data);
+	init(data);
+	animate();
+	render();
 	$(window).trigger('JSONready');
 });
 
@@ -10,55 +13,75 @@ $(window).on('JSONready', function () {
 	alert(data.length);
 });
 
+//My true global var homies
 var scene, camera, light, renderer, controls;
 
-init();
-animate();
+
 
 //this function initializes the 3D world it creatres the scene, objects, lights and render engine
-function init() {
+function init(data) {
 
 	//Le scene AKA the world, objects, and environment stuffs
 	scene = new THREE.Scene();
-	var geometry = new THREE.OctahedronGeometry(10, 1, 1);
-	var material = new THREE.MeshPhongMaterial({ color: 0x057075, specular: 0x009900, shininess: 30, shading: THREE.FlatShading });
+	var geometry = new THREE.OctahedronGeometry(2, 1, 1);
+	
 
-	for (var i = 0; i < data.length; i++) {
+	//var textGeom = new THREE.TextGeometry('Century', {font:'century gothic', weight: 'normal'});
+	var material = new THREE.MeshPhongMaterial({ color: 0xCCCCCC, specular: 0x009900, shininess: 30, shading: THREE.FlatShading });
+
+	for (var i = 0; i <  data.length; i++) {
 		var polygon = new THREE.Mesh(geometry, material);
-		polygon.position.x = (Math.random() - 0.5) * 1000;
-		polygon.position.y = (Math.random() - 0.5) * 1000;
-		polygon.position.z = (Math.random() - 0.5) * 1000;
+		var day = data[i].Day;
+		var daySplit = day.split("/");
+		polygon.position.x = (data[i].average) *.02;
+		polygon.position.y = (daySplit[0]) *10;
+		polygon.position.z = (daySplit[1]) *10;
 		polygon.updateMatrix();
 		polygon.matrixAutoUpdate = false;
 		scene.add(polygon);
-				}
+	}
 
-	//Camera and TrackBall camera controls	
+	//Camera and TrackBall camera controls REMEMBER TO LOOK INTO SMOOTHING OUT PANNING!!!!	
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-	camera.position.z = 5;
+	camera.position.z = 50;
 
 	controls = new THREE.TrackballControls(camera); //use TrackBall.js file
-	controls.keys = [65, 83, 68];
+	controls.keys = [65, 83, 68]; //have to double check keey bindings, shits not working
 
 	controls.addEventListener('change', render);
 
 	//Let there be lights
-				light = new THREE.DirectionalLight(0xffffff);
-				light.position.set(1, 1, 1);
-				scene.add(light);
-				light = new THREE.DirectionalLight(0x002288);
-				light.position.set(-1, -1, -1);
-				scene.add(light);
-				light = new THREE.AmbientLight(0x222222);
-				scene.add(light)
+	light = new THREE.DirectionalLight(0xffffff);
+	light.position.set(1, 1, 1);
+	scene.add(light);
+	light = new THREE.DirectionalLight(0x002288);
+	light.position.set(-1, -1, -1);
+	scene.add(light);
+	light = new THREE.AmbientLight(0x222222);
+	scene.add(light)
+
+	//add some fog to give a better feel for depth/distance of objects. Fog is exponential
+	scene.fog = new THREE.FogExp2(0x057075, .0050)
 
 	//the renderer the power!!!!
 	renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor( scene.fog.color );
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
-}
 
+	//Below fires off on resize, might add another trigger....
+	window.addEventListener('resize', onWindowResize, false);
+
+}
+//Sets the camera aspect ratio, updates what is looking at then changes the size of the projection to screen size and updates TrackBall mouse controls to reflect that
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	controls.handleResize();
+	render();
+}
 
 //The function below will request an animation frame if things change in the scene then update the cam controls 
 function animate() {
@@ -68,7 +91,6 @@ function animate() {
 
 //re-paints or creates the world upon request and updates the camera view
 function render() {
-				renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
 
-render();
